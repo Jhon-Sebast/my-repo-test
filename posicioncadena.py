f8295f2e97603f7326b1628b2a20c38d66f1852b
Jhon Sebastian Cordoba Riascos <jhonsebastiancordobariascos@MacBook-Pro-de-Jhon.local>
2025-02-20 17:36:40 -0500

    primer commit: agrego posiciones

---
+++ posicioncadena.py
@@ -0,0 +1,12 @@
+#obtener la posicion de un caracter
+sentence='Hola mundo'
+print(sentence[0])
+print(sentence[1])
+
+#trocear una cadena
+proverb='agua pesada no mueve molino'
+print(proverb[:])
+print(proverb[12:])
+print(proverb[:11])
+print(proverb[5:11])
+print(proverb[5:11:2])
